1bab1a30e609f56d8c15b0aa05afa5abd37b0855
Itai <itaiezra@gmail.com>
2012-12-02 22:42:41 +0200

    Last changes, still cannot connect

---
+++ login.php
@@ -0,0 +1,46 @@
+<?php
+
+session_start();
+
+$username = $_post['username'];
+$password = $_post['password'];
+
+if ($username&&$password)
+{
+	$connect = mysql_connect("mysql16.joinweb.co.il:3306", "whosinco", "YcNsIe13") or die ("Couldn't connect to database");
+	mysql_select_db("whosinco_users1") or die ("couldn't find database");
+	
+	$query = mysql_query("SELECT * FROM users WHERE Email='$username'");
+	
+	$numrows = mysql_num_rows($query);
+	
+	if($numrows != 0)
+	
+	{
+		
+		while ($row = mysql_fetch_assoc($query))
+		{
+			$dbusername = $row['username'];
+			$dbpassword = $row['password'];			
+		}
+		
+			if ($username==$dbusername&&$password==$dbpassword)
+			{
+				echo "Login successful. <a href='membersarea.php'>Click here to enter the members area</a>";
+				$_SESSION['username']=$dbusername;
+			
+			}
+			else 
+				echo "Incorrect password";
+				
+		
+		
+	}
+	else 
+		die ("That email does not exist");
+	
+}
+else 
+	die ("Please enter an email and password");
+
+?>
