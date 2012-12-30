eba17b0ab4e80da1928b99560da972cf9141ac56
Itai <itaiezra@gmail.com>
2012-12-30 21:55:18 +0200

    small change to app_re.php

--- WEB/app_re.php
+++ WEB/app_re.php
@@ -1,20 +1,23 @@
 <?php
-$list = array();
+//$list = array();
 $count = 1;
 require("includes/mysql_connect.php");
 $query = mysql_query("SELECT * FROM re");
 $numrows = mysql_num_rows($query);
 		while ($row = mysql_fetch_assoc($query))
 		{
-			$list['$count'] = $row['ID'];
+			$list[] = $row;
 			$count++;	
 		}
 if($numrows)
-	while($count!=1)
+	/*while($count!=1)
 	{
 		$count--;
 		echo $list['$count'];
 	}
+	*/
+	$list[]=$row;
+	echo json_encode($list);
 mysql_free_result($query);
 
 ?>
--- app_re.php
+++ app_re.php
@@ -1,12 +1,12 @@
 <?php
-$list = array();
+//$list = array();
 $count = 1;
 require("includes/mysql_connect.php");
 $query = mysql_query("SELECT * FROM re");
 $numrows = mysql_num_rows($query);
 		while ($row = mysql_fetch_assoc($query))
 		{
-			$list['$count'] = $row['ID'];
+			$list[] = $row;
 			$count++;	
 		}
 if($numrows)
@@ -16,7 +16,8 @@ if($numrows)
 		echo $list['$count'];
 	}
 	*/
-	echo json_encode($count);
+	$list[]=$row;
+	echo json_encode($list);
 mysql_free_result($query);
 
 ?>
