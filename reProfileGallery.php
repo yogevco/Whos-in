378f42fbb1e23e83eefcee727aa87c24ff75099d
Itai <itaiezra@gmail.com>
2013-01-06 17:05:37 +0200

    rearange Gallery page

--- reProfileGallery.php
+++ reProfileGallery.php
@@ -6,7 +6,7 @@
 	$dbName = $_SESSION['Name'];
 	$dbAdress = $_SESSION['Adress'];
 	$dbEmail = $_SESSION['Email'];
-	$TARGET_PATH = "images/";
+	$TARGET_PATH .= "images/";
  	$TARGET_PATH .= $dbLOGO;
 
 	if($_SESSION['Slide_show'] == "off")
@@ -29,7 +29,19 @@
 		$numrows = mysql_num_rows($query);
 		while ($row = mysql_fetch_assoc($query))
 		{
-			$_SESSION[$count] = $row['Picture'];
+		  $dbPicture = $row['Picture'];
+		  $TP = $dbPicture;
+		  $kab = explode(".", $dbPicture);
+		  $format = end($kab);
+		
+		  require("includes/ak_php_ing_lib_1.0.php");
+	//	$target_file = "uploads/$fileName";
+	//	$resized_file = "uploads/resized_$fileName";
+        $wmax = 350;
+        $hmax = 250;
+        ak_img_resize($TP, $TP, $wmax, $hmax, $format);
+			$_SESSION[$count] = $TP;
+			//$_SESSION[$count] = $row['Picture'];
 			$count = $count + 1;
 		}
 			return $count;
@@ -39,26 +51,28 @@
 	function run($count, $curr)
 	{
 		$last = $count;
-		if($curr > $last)
+		if($curr >= $last)
 			$curr = 1;
 
-		if(($last-$curr)<9)
-			$curr=$last-9;
-				
-		if($last<15)
+		if(($last>10)&&(($last-$curr)<9))
+			$curr=$last-10;
+		$j=$last;	
+		if($last<10)
 		{ 
 			$j=$last;
-			while (j!=16)
+			while ($j!=10)
 			{
 				$_SESSION[$j]=NULL;
-				$j++;
+				$j = $j + 1;
 			}
 		}
+	//	echo "curr is: $curr last is: $last j is: $j ";
 		return $curr;
 	}
 			
 	$_SESSION['Slide_show'] = "on";
 	$_SESSION['curr'] = $curr;
+	 
 ?>
 
 <html>
@@ -90,7 +104,7 @@
 			<li>        
    				<?php
 				$temp=5;
-				$loc=0;
+				$loc=1;
 				while($temp!=0)
 				{
 					if($_SESSION[$curr+$loc]==NULL)
@@ -109,15 +123,22 @@
     	</ul>
     </div>
     <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
     <div id="gallerywrapper">
- 		 <ul class="gallerydisplay">
+<ul class="gallerydisplay">
 			<li>        
    				<?php
 				$temp=5;
 				$loc=5;
 				while($temp!=0)
 				{
-					if($_SESSION[$curr+$loc]===NULL)
+					if($_SESSION[$curr+$loc]==NULL)
 					{
 						echo "<img src='".$TARGET_PATH."'>";
 					}
@@ -132,12 +153,31 @@
 			</li>
     	</ul>
     </div>
+  
     <p>&nbsp;</p>
-<form action="logout.php" method ="POST">
-  <button type="submit" class="btn signup-btn">
-        	logout
-  </button>
-</form>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <div id="gallerywrapper">
+ 		 <ul class="gallerydisplay">
+			<li>        
+   			  <form action="logout.php" method ="POST">
+			  <button type="submit" class="btn signup-btn">
+        		logout
+  			  </button>
+			  </form>
+			</li>
+   	  </ul>
+</div>
+
 <meta http-equiv="refresh" content="10">
 </body>
 </html>
